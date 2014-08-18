dd245acab338f6413f234b52fd8e573c9360082d
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-08-18 13:32:22 +0400

    fixed lists.py tests

--- introduction_course/lesson4/task1/tests.py
+++ introduction_course/lesson4/task1/tests.py
@@ -3,7 +3,7 @@ from test_helper import run_common_tests, get_task_windows, passed, failed, get_
 
 def test_value():
     output = get_file_output()
-    if output.contains("[4, 9, 16]"):
+    if "[4, 9, 16]" in output:
         passed()
     else:
         failed("Use list slicing lst[index1:index2]")
