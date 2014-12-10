2466c7539a5facbbcf8a3d2ad40b5388eb712b07
Liana Bakradze <liana.bakradze@jetbrains.com>
2014-12-10 19:16:18 +0300

    EDU-281 Incomplete test for task: lesson 9 task 2

--- introduction_course/lesson9/task2/tests.py
+++ introduction_course/lesson9/task2/tests.py
@@ -1,12 +1,15 @@
-from test_helper import run_common_tests, failed, passed, get_answer_placeholders
+from test_helper import run_common_tests, failed, passed, get_answer_placeholders, get_file_output
 
 
 def test_window():
     window = get_answer_placeholders()[0]
-    if "datetime" in window:
+    output = get_file_output()
+    if "datetime" not in window:
+        failed("Use datetime module")
+    elif len(output) > 0 and "<" not in output[0]:
         passed()
     else:
-        failed("Use datetime module")
+        failed("Print current date")
 
 if __name__ == '__main__':
     run_common_tests()
