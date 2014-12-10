d1adb614d4d77c9f28286e00b6f4da702acdf5a1
Liana Bakradze <liana.bakradze@jetbrains.com>
2014-12-10 15:00:14 +0300

    EDU-297 incomplete test for lesson 6 task 3

--- introduction_course/lesson6/task3/tests.py
+++ introduction_course/lesson6/task3/tests.py
@@ -1,4 +1,4 @@
-from test_helper import run_common_tests, failed, passed, get_answer_placeholders
+from test_helper import run_common_tests, failed, passed, get_answer_placeholders, get_file_output
 
 
 def test_column():
@@ -16,6 +16,7 @@ def test_window():
     else:
         failed("Use while loop to iterate")
 
+
 def test_window1():
     window = get_answer_placeholders()[0]
     if "number" in window:
@@ -32,9 +33,22 @@ def test_window2():
         failed("Check that 'number' is strictly less than 10 in condition")
 
 
+def test_output():
+    output = get_file_output()
+    if "Finished" not in output:
+        failed()
+    border = output.index("Finished")
+    user_squares = output[border + 1:]
+    correct_answer = list(map(str, [x * x for x in range(1, 10)]))
+    if correct_answer == user_squares:
+        passed()
+    else:
+        failed("Wrong answer")
+
 if __name__ == '__main__':
     run_common_tests()
     test_window()
     test_window1()
     test_window2()
     test_column()
+    test_output()
