c6e6c168854a3cf7b4425893bb980ce6ec962c72
Ekaterina Tuzova <Ekaterina.Tuzova@jetbrains.com>
2014-12-17 17:45:04 +0300

    EDU-298 Unclear task description for lesson 6/ task 3

--- introduction_course/lesson6/task3/while_loop.py
+++ introduction_course/lesson6/task3/while_loop.py
@@ -1,15 +1,15 @@
-count = 1
+square = 1
 
-while count <= 10:
-    print(count)    # This code is executed 10 times
-    count += 1      # This code is executed 10 times
+while square <= 10:
+    print(square)    # This code is executed 10 times
+    square += 1      # This code is executed 10 times
 
 print("Finished")  # This code is executed once
 
-count = 0
+square = 0
 number = 1
 
 print all squares from 0 to 99
-    count = number ** 2
-    print(count)
+    square = number ** 2
+    print(square)
     number += 1
